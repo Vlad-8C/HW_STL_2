ed1801ae581bac0c739a2682bf78aa0dbffe2d28
Vladislv <vlad.gritscsenko2w@gmail.com>
2025-10-31 00:22:20 +0500

    Add files via upload

---
+++ 2/HW_STL_2_2.cpp
@@ -0,0 +1,32 @@
+﻿// Шаблонную ф-ию, которая печатает содержимое контейнера. Контейнер может быть любым
+//
+
+#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void print_container(const T& v)
+{
+    for (const auto& p : v)
+    {
+        std::cout << p << '\t';
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set{ "one", "two", "three", "four", "five" };
+    print_container(test_set);
+    
+    std::list<std::string> test_list{ "one", "two", "three", "four", "five" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector{ "one", "two", "three", "four", "five" };
+    print_container(test_vector);
+
+    return EXIT_SUCCESS;
+}
